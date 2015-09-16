d3d8ad589d5093b8d5c84146c9bda5e89daf512f
xgfone <xgfone@126.com>
2015-09-16 10:53:38 +0800

    Modify the config

--- bservices/config.py
+++ bservices/config.py
@@ -2,25 +2,23 @@
 
 from oslo_config import cfg
 from oslo_log import log
-from oslo_db import options
 
 CONF = cfg.CONF
 
-_DEFAULT_PROJECT = "example"
-_DEFAULT_SQL_CONNECTION = 'sqlite:///tmp/%s.sqlite' % _DEFAULT_PROJECT
-_DEFAULT_LOG_LEVELS = ['root=INFO', '%s=INFO' % _DEFAULT_PROJECT]
+_ROOTS = ["root"]
+_DEFAULT_LOG_LEVELS = ['root=INFO']
 _DEFAULT_LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
 
-def parse_args(argv, project=None, default_config_files=None,
-               default_log_format=None, default_log_levels=None,
-               default_sql_conn=None):
-    project = project if project else _DEFAULT_PROJECT
+def parse_args(argv, project, version=None, default_config_files=None,
+               default_log_format=None, default_log_levels=None):
+    if project not in _ROOTS:
+        _DEFAULT_LOG_LEVELS.append('%s=INFO' % project)
+        _ROOTS.append(project)
     log_fmt = default_log_format if default_log_format else _DEFAULT_LOG_FORMAT
     log_lvl = default_log_levels if default_log_levels else _DEFAULT_LOG_LEVELS
-    sql_conn = default_sql_conn if default_sql_conn else _DEFAULT_SQL_CONNECTION
 
     log.set_defaults(log_fmt, log_lvl)
     log.register_options(CONF)
-    options.set_defaults(CONF, connection=sql_conn, sqlite_db='%s.sqlite' % project)
-    CONF(argv[1:], project=project, default_config_files=default_config_files)
+    CONF(argv[1:], project=project, version=version,
+         default_config_files=default_config_files)
