290b23b63c4e0da5bd5ba7a56ca182fdc8466805
xgfone <xgfone@126.com>
2015-09-25 12:40:41 +0800

    Correct a error.

--- bservices/examples/example_server.py
+++ bservices/examples/example_server.py
@@ -55,7 +55,7 @@ class Loader(object):
 
 
 def main():
-    config.parse_args(sys.argv)
+    config.parse_args(sys.argv, PROJECT)
     logging.setup(CONF, PROJECT)
     eventlet.monkey_patch(all=True)
 
