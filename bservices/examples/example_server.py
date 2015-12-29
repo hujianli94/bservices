67df629329f32a49c0dcae9b8b2f2288a96bb5f2
xgfone <xgfone@126.com>
2015-12-29 11:17:33 +0800

    Update the usage.

--- bservices/examples/example_server.py
+++ bservices/examples/example_server.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 """
-Server
+WSGI Server
     Run:
         python -m bservices.examples.example_server
 
-Client:
+HTTP Client:
     Add:
         curl http://127.0.0.1:10000/set_data -d '{"data": "test_data"}' -H "Content-Type: application/json"
         >>> {"id": 1}
@@ -12,15 +12,55 @@ Client:
         curl http://127.0.0.1:10000/get_data?id=1
         >>> {"data": "test_data", "id": 1}
 
-Controller API:
-    A action can return:
-        1) a dict object
-        2) a bservices.wsgi.ResponseObject object
-        3) a exception based on webob.exc.HTTPException or bservices.exception.ExceptionBase
-        4) any object compatible with webob.dec.wsgify.__call__, for example,
-            None, string(str, bytes, unicode), webob.Response, etc.
-    Notice:
-        The first two will be serialized, see bservices.wsgi.ResponseObject.
+Restriction of `bservices.wsgi.Resource`:
+    (1) Only support three kinds of Content-Type:
+        A. text/plain
+        B. application/json
+        C. application/xml
+    (2) Automatically serialize and deserialize the body of the request,
+        but only for application/json, text/plain. If you want to receive and
+        handle the XML content, you MUST register the serializer and deserializer
+        of XML; or, it will raise a exception.
+
+Controller Action API:
+    If using the wrapper of `Resource`, you obey the action rules below.
+    Or, you MUST implement the engine to handle the HTTP request.
+
+    The logic of `Resource` is simply.
+    1. It gets the controller, the action method, the URL pattern argument
+       by the result that `routes` parsed.
+    2. deserialize the body, if any.
+    3. pass all the arguments to the action method and call it.
+    4. handle the result.
+
+    Handle the result the action method returned:
+    (1) if a dict, serialize it to JSON.
+    (2) if a object of bservices.wsgi.ResponseObject, serialize it by default.
+    (3) If any others, hand it to webob.dec.wsgify to handle.
+
+    In the meanwhile, it also handle some convenient functions, such as the
+    default status code, see `bservices.wsgi.Resource._process_stack` and
+    `bservices.wsgi.response`. Moreover, it support the action mapping and the
+    extensions.
+
+    Action API:
+        Args:
+            (1) MUST have a argument, req, which is a object of
+                `bservices.wsgi.Request`.
+            (2) Arguments that `routes` parsed will be past to the action method.
+            (3) If having a body, the action method MUST supply a argument, body.
+
+        Return:
+            (1) a dict object
+            (2) a bservices.wsgi.ResponseObject object
+            (3) a exception based on webob.exc.HTTPException or
+                bservices.exception.ExceptionBase
+            (4) any object compatible with webob.dec.wsgify.__call__, for
+                example, None, string(str, bytes, unicode), webob.Response, etc.
+
+            Notice:
+                The first two will be serialized.
+                see bservices.wsgi.ResponseObject.
 """
 import logging
 import multiprocessing
